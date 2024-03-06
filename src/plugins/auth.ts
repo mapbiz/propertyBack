54b3a54ea64cb622096b257d7f13c86bf9c5f85f
Unkown496 <kroloeden@gmail.com>
2024-03-06 14:37:03 +0300

    Мелкий фикс полей

--- src/plugins/auth.ts
+++ src/plugins/auth.ts
@@ -1,7 +1,7 @@
 import bearer from "@elysiajs/bearer";
 import { Elysia } from "elysia";
 
-const authPlugin = new Elysia()
+const AuthPlugin = new Elysia()
 .use(bearer())
 .decorate("auth", {})
 .onRequest(({ request, headers, set }) => {
