d31ca777b0d1af05cdb42bbc1e4690e7dd6d39ad
Unkown496 <kroloeden@gmail.com>
2024-04-23 14:10:42 +0300

    Фикс слагификатора

--- src/helpers/slug.ts
+++ src/helpers/slug.ts
@@ -1,11 +1,20 @@
 import slugify from "slugify";
 
+slugify.extend({ 
+   '#': 'grid',
+   '@': 'at',
+   '&': 'an',
+   '%': 'percent',
+   '$': 'dollar',
+   ' ': 'unbreak-space',
+})
+
 export const slug = (string: string) => slugify(
    string,
    {  
       lower: true,
       replacement: "-",
       trim: true,
-      remove: /[*+~.()'"!:@?!,:;\[\]\\|\/_-`]/gm,
+      remove: /[*+~.()'"!:@?!,:;\[\]\\|\/_-`{}%]/gm,
    }  
 )  
